f45c9820eeb2df54d6b29d55462ab58e594c061d
Vishwang Dave <vishwangdave@gmail.com>
2020-11-11 13:53:12 +0530

    1. created the account app, and its required structure. set up basic structure of the entire web app and created base templates. 2. added few more requiremnts 3. made the default migrations to the app.

--- tumblr_clone/account/urls.py
+++ tumblr_clone/account/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+app_name = 'account'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/',include('account.urls')),
-    path('',views.IndexView.as_view(),name='index'),
+    path('',views.HomeView.as_view(),name='home'),
 ]
