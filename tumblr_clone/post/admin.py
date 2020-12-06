ec6ea0a567419dffb790c24ae5a8193ff7cb022d
Vishwang Dave <vishwangdave@gmail.com>
2020-12-06 17:27:32 +0530

    added complete posting relations and new fields to post

--- tumblr_clone/post/admin.py
+++ tumblr_clone/post/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Notes, Likes, Reblogs
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Likes)
+admin.site.register(Notes)
+admin.site.register(Reblogs)
